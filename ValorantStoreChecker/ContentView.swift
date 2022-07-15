71876d22f4ce92c4f786dd7c526383a0b09f642c
SoloUnity <GordonAccount@yahoo.com>
2022-07-15 12:08:40 -0400

    Initial Commit

---
+++ ValorantStoreChecker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ValorantStoreChecker
+//
+//  Created by Gordon Ng on 2022-07-15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ValorantStoreChecker/ValorantStoreCheckerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ValorantStoreCheckerApp.swift
+//  ValorantStoreChecker
+//
+//  Created by Gordon Ng on 2022-07-15.
+//
+
+import SwiftUI
+
+@main
+struct ValorantStoreCheckerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
